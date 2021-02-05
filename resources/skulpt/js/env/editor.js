a9f6c8ef920c04f6cb0b55832e0c67608b95ca68
Rahul Gopinath <rahul@gopinath.org>
2021-02-05 00:56:58 -0800

    .

--- resources/skulpt/js/env/editor.js
+++ resources/skulpt/js/env/editor.js
@@ -1,103 +1,21 @@
 $(document).ready(function () {
-//    var output = $('#edoutput');
-//    var outf = function (text) {
-//        output.text(output.text() + text);
-//    };
-//
-//    var keymap = {
-//        "Ctrl-Enter" : function (editor) {
-//            Sk.configure({output: outf,
-//                 read: builtinRead,
-//                 __future__: Sk.python3});
-//            Sk.canvas = "mycanvas";
-//            if (editor.getValue().indexOf('turtle') > -1 ) {
-//                $('#mycanvas').show()
-//            }
-//            Sk.pre = "edoutput";
-//            (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas';
-//            try {
-//                Sk.misceval.asyncToPromise(function() {
-//                    return Sk.importMainWithBody("<stdin>",false,editor.getValue(),true);
-//                });
-//            } catch(e) {
-//                outf(e.toString() + "\n")
-//            }
-//        },
-//        "Shift-Enter": function (editor) {
-//            Sk.configure({output: outf,
-//                read: builtinRead,
-//                __future__: Sk.python3});
-//            Sk.canvas = "mycanvas";
-//            Sk.pre = "edoutput";
-//            if (editor.getValue().indexOf('turtle') > -1 ) {
-//                $('#mycanvas').show()
-//            }
-//            try {
-//                Sk.misceval.asyncToPromise(function() {
-//                    return Sk.importMainWithBody("<stdin>",false,editor.getValue(),true);
-//                });
-//            } catch(e) {
-//                outf(e.toString() + "\n")
-//            }
-//        }
-//    }
-//
-//
-//    var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
-//        parserfile: ["parsepython.js"],
-//        autofocus: true,
-//        theme: "solarized dark",
-//        //path: "static/env/codemirror/js/",
-//        lineNumbers: true,
-//        textWrapping: false,
-//        indentUnit: 4,
-//        height: "100px",
-//        fontSize: "8pt",
-//        autoMatchParens: true,
-//        extraKeys: keymap,
-//        parserConfig: {'pythonVersion': 2, 'strictErrors': true}
-//    });
-//
-//    $("#skulpt_run").click(function (e) { keymap["Ctrl-Enter"](editor)} );
-//
-//    $("#toggledocs").click(function (e) {
-//        $("#quickdocs").toggle();
-//    });
-//
-//    var exampleCode = function (id, text) {
-//        $(id).click(function (e) {
-//            editor.setValue(text);
-//            editor.focus(); // so that F5 works, hmm
-//        });
-//    };
-//
-//    exampleCode('#codeexample1', "print(\"Hello, World!\")");
-//    $('#clearoutput').click(function (e) {
-//        $('#edoutput').text('');
-//        $('#mycanvas').hide();
-//    });
-//
-//
+
 function builtinRead(x) {
     if (Sk.builtinFiles === undefined || Sk.builtinFiles["files"][x] === undefined)
         throw "File not found: '" + x + "'";
     return Sk.builtinFiles["files"][x];
 }
 
-
-function runit(prefix, editor) { 
-   //var prog = document.getElementById("yourcode" + prefix).value;
+function runit(mypre, mycanvas, editor) {
    var prog = editor.getValue();
-   var mypre = document.getElementById("output" + prefix);
 
    function outf(text) {
-      var mypre = document.getElementById("output" + prefix);
       mypre.innerHTML = mypre.innerHTML + text;
    }
    mypre.innerHTML = '';
-   Sk.pre = "output" + prefix;
+   Sk.pre = mypre.id
    Sk.configure({output:outf, read:builtinRead, retainglobals: true, __future__: Sk.python3});
-   (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = 'mycanvas' + prefix;
+   (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = mycanvas.id
    var myPromise = Sk.misceval.asyncToPromise(function() {
        return Sk.importMainWithBody("<stdin>", false, prog, true);
    });
@@ -109,15 +27,20 @@ function runit(prefix, editor) {
    });
 }
 
-function getCodeMirror(prefix) {
-   return CodeMirror.fromTextArea(document.getElementById("yourcode" + prefix), { lineNumbers: true });
-}
-// initialize the codemirror first.
-var editor1 = getCodeMirror("1");
-var editor2 = getCodeMirror("2");
+$('[name="python_edit"]').each(function(idx) {
+   var editor = CodeMirror.fromTextArea($(this)[0], { lineNumbers: true });
+   $(this).data('CodeMirrorInstance', editor);
+});
 
-$('#button1').click(function() { runit("1", editor1); });
-$('#button2').click(function() { runit("2", editor2); });
+$('[name="python_run"]').click(function() { 
+  myform = $(this).closest('[name="python_run_form"]');
+  //myeditor = myform.find('[name="python_edit"]').next('.CodeMirror').get(0)
+  myeditor = myform.find('[name="python_edit"]').data('CodeMirrorInstance')
+  mypre = myform.find('[name="python_output"]').first()
+  mycanvas = myform.find('[name="python_canvas"]').first()
+  runit(mypre[0], mycanvas[0], myeditor)
+  // var editor = document.querySelector('.CodeMirror').CodeMirror;
+});
 
 //    editor.focus();
 });
