69eadf9f94bec7542d9aec5e084c90927e7f379b
Rahul Gopinath <rahul@gopinath.org>
2021-02-05 21:14:01 -0800

    .

--- resources/pyodide/js/env/editor.js
+++ resources/pyodide/js/env/editor.js
@@ -11,33 +11,6 @@ function runit(mypre, mycanvas, editor) {
    }
 
    mypre.innerHTML = '';
-   // Sk.pre = mypre.id
-   // Sk.configure({output:outf, read:builtinRead, retainglobals: true, __future__: Sk.python3});
-   // (Sk.TurtleGraphics || (Sk.TurtleGraphics = {})).target = mycanvas.id
-   // var myPromise = Sk.misceval.asyncToPromise(function() {
-   //    return Sk.importMainWithBody("<stdin>", false, prog, true);
-   //});
-   //
-   languagePluginLoader.then(() => { 
-      console.log('pyodide ready');
-      $('[name="python_run"]').click(function() { 
-        myform = $(this).closest('[name="python_run_form"]');
-        myeditor = myform.find('[name="python_edit"]').data('CodeMirrorInstance')
-        mypre = myform.find('[name="python_output"]').first()
-        mycanvas = myform.find('[name="python_canvas"]').first()
-        runit(mypre[0], mycanvas[0], myeditor)
-      });
-
-      $('[name="python_run_all"]').click(function() { 
-        $(document).find('[name="python_run_form"]').each(function(idx, myform_) {
-        myform = $(myform_)
-        myeditor = myform.find('[name="python_edit"]').data('CodeMirrorInstance')
-        mypre = myform.find('[name="python_output"]').first()
-        mycanvas = myform.find('[name="python_canvas"]').first()
-        runit(mypre[0], mycanvas[0], myeditor)
-        });
-      });
-   });
 
    pyodide.runPythonAsync(prog)
         .then(output => {
@@ -50,15 +23,6 @@ function runit(mypre, mycanvas, editor) {
            errf(err.toString())
            editor.display.wrapper.style.border = '1px solid red'
         });
-   //myPromise.then(function(mod) {
-   //    console.log('success');
-   //    editor.display.wrapper.style.border = '1px solid black'
-   //},
-   //    function(err) {
-   //    console.log(err.toString());
-   //    errf(err.toString())
-   //    editor.display.wrapper.style.border = '1px solid red'
-   //});
 }
 
 $('[name="python_edit"]').each(function(idx) {
@@ -66,4 +30,27 @@ $('[name="python_edit"]').each(function(idx) {
    $(this).data('CodeMirrorInstance', editor);
 });
 
-});
+languagePluginLoader.then(() => { 
+  console.log('pyodide ready');
+
+  $('[name="python_run"]').click(function() { 
+    myform = $(this).closest('[name="python_run_form"]');
+    myeditor = myform.find('[name="python_edit"]').data('CodeMirrorInstance')
+    mypre = myform.find('[name="python_output"]').first()
+    mycanvas = myform.find('[name="python_canvas"]').first()
+    runit(mypre[0], mycanvas[0], myeditor)
+  });
+
+  $('[name="python_run_all"]').click(function() { 
+    $(document).find('[name="python_run_form"]').each(function(idx, myform_) {
+    myform = $(myform_)
+    myeditor = myform.find('[name="python_edit"]').data('CodeMirrorInstance')
+    mypre = myform.find('[name="python_output"]').first()
+    mycanvas = myform.find('[name="python_canvas"]').first()
+    runit(mypre[0], mycanvas[0], myeditor)
+    });
+  });
+
+}); //lang plugin
+
+}); //document
