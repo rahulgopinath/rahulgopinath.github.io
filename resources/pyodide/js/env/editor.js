f51d412d620f9b80e048cee7d5cd022ec201d0d1
Rahul Gopinath <rahul@gopinath.org>
2021-02-05 22:15:13 -0800

    .

--- resources/pyodide/js/env/editor.js
+++ resources/pyodide/js/env/editor.js
@@ -39,7 +39,7 @@ $('[name="python_edit"]').each(function(idx) {
 
 languagePluginLoader.then(() => { 
   console.log('pyodide ready');
-  var pre =  "\nimport io, sys\n__IODIDE_console=sys.stdout\n'
+  var pre =  '\nimport io, sys\n__IODIDE_console=sys.stdout\n'
   var pre_ = '\ndef _dbg(v): print(v, file=__IODIDE_console)\n'
 
   pyodide.runPython(pre + pre_)
