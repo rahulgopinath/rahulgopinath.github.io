037d122f8821572ec9e6bb27abe35b474cbd1eb3
Rahul Gopinath <rahul@gopinath.org>
2021-02-07 06:25:26 -0800

    .

--- e.py
+++ e.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python3
 import sys
 from html import escape
-clean_html = sys.stdin.read()
+clean_html = sys.stdin.read().strip()
+if clean_html[0:4] == '```': clean_html = clean_html[4:]
+if clean_html[-3:] == '```': clean_html = clean_html[:-3]
 scraped_html = escape(clean_html)
 print('''
-<form name='python_run_form'>
-<textarea cols="40" rows="4" name='python_edit'>
-''')
-print(scraped_html)
+<!--
+############
+%s
+############
+-->
+''' % clean_html)
 print('''
+<form name='python_run_form'>
+<textarea cols="40" rows="4" name='python_edit'>''')
+print(scraped_html)
+print('''\
 </textarea><br />
 <button type="button" name="python_run">Run</button>
 <pre class='Output' name='python_output'></pre>
