ff4c6ba21602e005f3e1c028410bbcb734f93e0c
Rahul Gopinath <rahul@gopinath.org>
2021-11-10 03:27:32 -0800

    .

---
+++ pkg/P/__init__.py
@@ -0,0 +1,11 @@
+"""
+P.
+
+An example P
+"""
+
+__version__ = '0.0.1'
+__author__ = 'Rahul Gopinath'
+
+
+from post import *
---
+++ pkg/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='P',
+    version='0.0.1',    
+    description='D',
+    url='https://github.com/rahulgopinath/rahulgopinath.github.io',
+    author='Rahul Gopinath',
+    author_email='rahul@gopinath.org',
+    license='Fuzingbook',
+    packages=['P'],
+    install_requires=['sympy==1.8'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 3',
+    ],
+)
