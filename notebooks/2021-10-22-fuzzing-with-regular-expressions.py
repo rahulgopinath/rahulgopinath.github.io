6e60a1d792f4d5c919590b77cf189ccfd71cc8a4
Rahul Gopinath <rahul@gopinath.org>
2021-11-10 16:55:59 -0800

    .

--- notebooks/2021-10-22-fuzzing-with-regular-expressions.py
+++ notebooks/2021-10-22-fuzzing-with-regular-expressions.py
@@ -27,6 +27,7 @@
 # https://rahul.gopinath.org/py/earleyparser-0.0.1-py2.py3-none-any.whl
 # https://rahul.gopinath.org/py/hdd-0.0.1-py2.py3-none-any.whl
 # https://rahul.gopinath.org/py/pegparser-0.0.1-py2.py3-none-any.whl
+# https://rahul.gopinath.org/py/ddset-0.0.1-py2.py3-none-any.whl
 
 # The imported modules
 
