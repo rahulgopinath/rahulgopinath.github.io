4ea221b3b09aee1ce6ac9cb169ce3b86a6b9822f
Rahul Gopinath <rahul@gopinath.org>
2021-11-13 15:14:02 -0800

    .

--- notebooks/2021-11-13-regular-grammar-to-regular-expression.py
+++ notebooks/2021-11-13-regular-grammar-to-regular-expression.py
@@ -32,6 +32,7 @@
 # https://rahul.gopinath.org/py/ddset-0.0.1-py2.py3-none-any.whl
 # https://rahul.gopinath.org/py/rxfuzzer-0.0.1-py2.py3-none-any.whl
 # https://rahul.gopinath.org/py/rxregular-0.0.1-py2.py3-none-any.whl
+# https://rahul.gopinath.org/py/rxcanonical-0.0.1-py2.py3-none-any.whl
 
 # The imported modules
 
@@ -40,25 +41,51 @@ import gatleastsinglefault as gatleast
 #import earleyparser
 #import rxfuzzer
 #import rxregular
+import rxcanonical
 import itertools as I
 
-# Converting a regular gramamr to a regular expression is fairly simple. You
-# start at the start symbol and start flattening.
-
-EMPTY_KEY = "<_>"
 
 MY_RGRAMMAR = {"<S>": [["a", "<A>"]],
     "<A>" : [
-        ["<_>"],
+        [rxcanonical.NT_EMPTY],
         ["b","<S>"],
         ["b","<A>"],
         ["a","<B>"],
     ],
     "<B>" : [
-        ["b","<_>"],
+        ["b",rxcanonical.NT_EMPTY],
         ["a","<S>"]
     ],
-    "<_>" : [[]]}
+    rxcanonical.NT_EMPTY : [[]]}
+
+# Regularize the grammar
+# The grammar should have one start symbol
+# and exactly one stop symbol which is NT_EMPTY
+
+def fix_grammar(g, empty_nt=rxcanonical.NT_EMPTY):
+    assert empty_nt in g
+    new_g = {}
+    for k in g:
+        new_rules = []
+        for rule in g[k]:
+            if len(rule) == 1:
+                if fuzzer.is_nonterminal(rule[0]):
+                    assert rule[0] == empty_nt
+                    new_rules.append(rule)
+                else:
+                    new_rules.append([rule[0], empty_nt])
+            else:
+                new_rules.append(rule)
+        new_g[k] = new_rules
+    return new_g
+
+# 
+
+if __name__ == '__main__':
+    g = fix_grammar(MY_RGRAMMAR)
+    gatleast.display_grammar(g, "<S>")
+
+# 
 
 def is_nonterminal(item):
     if not isinstance(item, str): return False
@@ -71,17 +98,7 @@ def split_regex_prefix(rule):
         else: prefix += 1
     return rule[:prefix], rule[prefix:]
 
-def refine(rule):
-    prefix, nts = split_regex_prefix(rule)
-    if len(nts) == 1:
-        return [("concat", *prefix), nts[0]]
-    elif len(nts) == 0:
-        return prefix
-    else:
-        assert False
-# 
-
-def produce_prefix_regex(rules, grammar):
+def produce_prefix_regex(rules, grammar, empty_nt=rxcanonical.NT_EMPTY):
     # convert <A> := a <B> | b <B> to (a|b) <B>
     # convert <A> := <_> to <A> := <_>
     # convert <_> := \e to <_> := \e
@@ -92,7 +109,7 @@ def produce_prefix_regex(rules, grammar):
         if rule == []:
             has_epsilon = True
             continue
-        if rule == [EMPTY_KEY]:
+        if rule == [empty_nt]:
             has_emptykey = True
             continue
         prefix, lnt = split_regex_prefix(rule)
@@ -103,27 +120,41 @@ def produce_prefix_regex(rules, grammar):
 
     new_rules = []
     if has_epsilon: new_rules.append([])
-    if has_emptykey: new_rules.append([EMPTY_KEY])
+    if has_emptykey: new_rules.append([empty_nt])
 
     for lnt in lnt_hash:
         if len(lnt_hash[lnt]) > 1:
-            rex = ("or", lnt_hash[lnt])
+            rex = ("or", *lnt_hash[lnt])
             new_rules.append([rex, lnt])
         else:
             rex = lnt_hash[lnt][0]
             new_rules.append([rex, lnt])
     return new_rules
+
+def g_produce_prefix_regex(grammar):
+    new_grammar = {}
+    for k in grammar:
+        new_rules = produce_prefix_regex(grammar[k], grammar)
+        new_grammar[k] = new_rules
+    return new_grammar
 # 
 if __name__ == '__main__':
     print()
-    print("Definition")
-    gatleast.display_grammar(MY_RGRAMMAR, "<S>")
-    print("<B>")
-    new_rules = produce_prefix_regex(MY_RGRAMMAR["<B>"], MY_RGRAMMAR)
-    print("New Rules:")
-    for r in new_rules:
-        print(r)
-    print()
+    rgrammar = fix_grammar(MY_RGRAMMAR)
+    new_rgrammar = g_produce_prefix_regex(rgrammar)
+    gatleast.display_grammar(new_rgrammar, "<S>")
+
+# 
+
+def refine(rule):
+    prefix, nts = split_regex_prefix(rule)
+    if len(nts) == 1:
+        return [("concat", *prefix), nts[0]]
+    elif len(nts) == 0:
+        return prefix
+    else:
+        assert False
+# 
 
 # dealing with recursion
 # A -> b B | c C | a A
@@ -141,69 +172,109 @@ def make_self_loops_to_star(rules, nt):
         nr = refine([("star", r_rule[0]), *r])
         new_rules.append(nr)
     return new_rules
-    
 
-def convert_definition_to_regex_prefix(nt, grammar):
-    rules = grammar[nt]
-    my_rules = produce_prefix_regex(rules, grammar)
-    my_rules_ = make_self_loops_to_star(my_rules, nt)
-    return my_rules_
-# 
+def g_make_self_loops_to_star(g):
+    new_g = {}
+    for k in g:
+        rules = make_self_loops_to_star(g[k], k)
+        new_g[k] = rules
+    return new_g
+   
+
 if __name__ == '__main__':
     print()
-    print("Definition")
-    gatleast.display_grammar(MY_RGRAMMAR, "<S>")
-    print("<A>")
-    new_rules = convert_definition_to_regex_prefix("<A>", MY_RGRAMMAR)
-    print("Rules:")
-    for r in new_rules:
-        print(r)
-    print()
+    new_rgrammar2 = g_make_self_loops_to_star(new_rgrammar)
+    gatleast.display_grammar(new_rgrammar2, "<S>")
 
-#
+# --
 
-def flatten_rule(rule, grammar, nt):
+def flatten_rule(rule, grammar, nt, empty_nt=rxcanonical.NT_EMPTY):
     # find the prefix without nonterminal
     prefix, nts = split_regex_prefix(rule)
     assert len(nts) <= 1
     if not nts: return [prefix]
     if nts[0] == nt: # recursion
+        assert False
         return [rule]
-    if nts[0] == EMPTY_KEY: # dont expand empty key
+    if nts[0] == empty_nt: # dont expand empty key
         return [rule]
     new_suffixes = grammar[nts[0]]
     return [refine(prefix+r) for r in new_suffixes]
 
+
+def eliminate_nt(grammar, nt, empty_nt=rxcanonical.NT_EMPTY):
+    new_g = {}
+    for k in grammar:
+        if k == nt: continue
+        new_rules = []
+        for r in grammar[k]:
+            if len(r) == 0:
+                # E -> \e
+                assert k == empty_nt
+                new_rules.append(r)
+            elif len(r) == 1:
+                # S -> E
+                assert r[0] == empty_nt
+                new_rules.append(r)
+            elif len(r) == 2:
+                if r[1] == nt:
+                    rs = flatten_rule(r, grammar, k)
+                    new_rules.extend(rs)
+                else:
+                    new_rules.append(r)
+            else: assert False
+        new_g[k] = new_rules
+    return new_g
+
+
 # Using it.
 
 if __name__ == '__main__':
-    gatleast.display_grammar(MY_RGRAMMAR, "<S>")
-    print("<B>", "::", MY_RGRAMMAR['<B>'][1])
-    rules = flatten_rule(MY_RGRAMMAR['<B>'][1], MY_RGRAMMAR, "<B>")
-    print("Expanded to:")
-    for r in rules:
-        print(r)
+    g = dict(new_rgrammar2)
+    new_g = eliminate_nt(g, '<A>')
+    gatleast.display_grammar(new_g, "<S>")
 
-# 
-def flatten_definition(rules, grammar, nt):
-    new_rules = []
-    for rule in rules:
-        new_rule_set = flatten_rule(rule, grammar, nt)
-        new_rules.extend(new_rule_set)
-    return new_rules
+# Eliminate each nonterminal one by one
 
-# 
+def convert_rex(rex):
+    if rex[0] == 'or':
+        return '|'.join([convert_rex(r) for r in rex[1:]])
+    elif rex[0] == 'concat':
+        return ''.join([convert_rex(r) for r in rex[1:]])
+    elif rex[0] == 'star':
+        v = convert_rex(rex[1])
+        if rex[1][0] not in ['concat', 'star', 'or']:
+            return "%s*" % v
+        return "(%s)*" % v
+    else:
+        return rex
+
+
+def rg_to_regex(grammar, start_nt, empty_nt=rxcanonical.NT_EMPTY):
+    fixedg = fix_grammar(grammar)
+    new_rgrammar = g_produce_prefix_regex(fixedg)
+    new_rgrammar2 = g_make_self_loops_to_star(new_rgrammar)
+
+    keys = [k for k in grammar if k not in [start_nt, empty_nt]]
+
+    g = dict(new_rgrammar2)
+    for k in keys:
+        g_ = eliminate_nt(g, k, empty_nt)
+        g = g_
+        g_ = g_make_self_loops_to_star(g)
+        g = g_
+    g_ = g_produce_prefix_regex(g)
+    assert len(g_[start_nt]) == 1
+    rex_rule = g_[start_nt][0]
+    assert rex_rule[1] == empty_nt
+    return convert_rex(rex_rule[0])
+
+# Using it.
+
 if __name__ == '__main__':
-    print("Definition")
-    gatleast.display_grammar(MY_RGRAMMAR, "<S>")
-    print("<B>")
-    b_rules = convert_definition_to_regex_prefix('<B>', MY_RGRAMMAR)
-    rules = flatten_definition(b_rules, MY_RGRAMMAR, '<B>')
-    print("Expanded to:")
-    for r in rules:
-        print(r)
-
-#  Now, pick a key and eleminate.
+    g = dict(MY_RGRAMMAR)
+    rx = rg_to_regex(g, '<S>')
+    print(rx)
 
 
 # The runnable code for this post is available
