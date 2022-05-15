a28228a75b75227f70b996194f1729e27457697a
Rahul Gopinath <rahul@gopinath.org>
2022-05-15 06:20:11 -0700

    .

--- notebooks/2022-05-15-f1.py
+++ notebooks/2022-05-15-f1.py
@@ -10,7 +10,14 @@
 # In a previous [post](/post/2019/05/28/simplefuzzer-01/) I disucssed a simple
 # fuzzer.  While simple, the fuzzer is somewhat inefficient. This post discusses
 # a way to speed it up -- by compiling.
-# 
+
+#@
+# https://rahul.gopinath.org/py/simplefuzzer-0.0.1-py2.py3-none-any.whl
+
+# The imported modules
+
+import simplefuzzer as fuzzer
+
 # As before, we start with a grammar.
 
 EXPR_GRAMMAR = {
@@ -37,134 +44,15 @@ EXPR_GRAMMAR = {
 
 EXPR_START = '<start>'
 
-# We define a simple interface for fuzzing.
-
-class Fuzzer:
-    def __init__(self, grammar):
-        self.grammar = grammar
-
-    def fuzz(self, key='<start>', max_num=None, max_depth=None):
-        raise NotImplemented()
-
-# Next, we define our limit fuzzer that we discussed in the previous post.
-
-class LimitFuzzer(Fuzzer):
-    def __init__(self, grammar):
-        super().__init__(grammar)
-        self.key_cost = {}
-        self.cost = self.compute_cost(grammar)
-
-# ##  Cost
-# The cost of a nonterminal symbol is the minimum cost of the rules that define
-# it. A terminal symbol costs a unit. The cost of a rule is the maximum cost of
-# all the symbols that are in the rule.
-# 
-# So, we first find the cost of expansion for each grammar rule.
-
-class LimitFuzzer(LimitFuzzer):
-    def compute_cost(self, grammar):
-        cost = {}
-        for k in grammar:
-            cost[k] = {}
-            for rule in grammar[k]:
-                cost[k][str(rule)] = self.expansion_cost(grammar, rule, set())
-            if len(grammar[k]):
-                assert len([v for v in cost[k] if v != float('inf')]) > 0
-        return cost
-
-# ### Symbol Cost
-
-class LimitFuzzer(LimitFuzzer):
-    def symbol_cost(self, grammar, symbol, seen):
-        if symbol in self.key_cost: return self.key_cost[symbol]
-        if symbol in seen:
-            self.key_cost[symbol] = float('inf')
-            return float('inf')
-        v = min((self.expansion_cost(grammar, rule, seen | {symbol})
-                    for rule in grammar.get(symbol, [])), default=0)
-        self.key_cost[symbol] = v
-        return v
-
-# ### Rule Cost
-class LimitFuzzer(LimitFuzzer):
-    def expansion_cost(self, grammar, tokens, seen):
-        return max((self.symbol_cost(grammar, token, seen)
-                    for token in tokens if token in grammar), default=0) + 1
-
-# ### Generating
-
-class LimitFuzzer(LimitFuzzer):
-    def is_nt(self, k):
-        return k and k[0], k[-1] == ('<', '>')
-
-    def nonterminals(self, rule):
-        return [t for t in rule if self.is_nt(t)]
-
-    def iter_gen_key(self, key, max_depth):
-        def get_def(t):
-            if t in ASCII_MAP:
-                return [random.choice(ASCII_MAP[t]), []]
-            elif t and t[-1] == '+' and t[0:-1] in ASCII_MAP:
-                num = random.randrange(FUZZRANGE) + 1
-                val = [random.choice(ASCII_MAP[t[0:-1]]) for i in range(num)]
-                return [''.join(val), []]
-            elif self.is_nt(t):
-                return [t, None]
-            else:
-                return [t, []]
-
-        cheap_grammar = {}
-        for k in self.cost:
-            rules = self.grammar[k]
-            if rules:
-                min_cost = min([self.cost[k][str(r)] for r in rules])
-                cheap_grammar[k] = [r for r in self.grammar[k] if self.cost[k][str(r)] == min_cost]
-            else:
-                cheap_grammar[k] = [] # (No rules found)
-
-        root = [key, None]
-        queue = [(0, root)]
-        while queue:
-            (depth, item), *queue = queue
-            key = item[0]
-            if item[1] is not None: continue
-            grammar = self.grammar if depth < max_depth else cheap_grammar
-            chosen_rule = random.choice(grammar[key])
-            expansion = [get_def(t) for t in chosen_rule]
-            item[1] = expansion
-            for t in expansion: queue.append((depth+1, t))
-
-        return root
-
-    def gen_key(self, key, depth, max_depth):
-        if key in ASCII_MAP:
-            return (random.choice(ASCII_MAP[key]), [])
-        if key and key[-1] == '+' and key[0:-1] in ASCII_MAP:
-            m = random.randrange(FUZZRANGE) + 1
-            return (''.join([random.choice(ASCII_MAP[key[0:-1]]) for i in range(m)]), [])
-        if key not in self.grammar: return (key, [])
-        if depth > max_depth:
-            clst = sorted([(self.cost[key][str(rule)], rule) for rule in self.grammar[key]])
-            rules = [r for c,r in clst if c == clst[0][0]]
-        else:
-            rules = self.grammar[key]
-        v = self.gen_rule(random.choice(rules), depth+1, max_depth)
-        return (key, v)
-
-    def gen_rule(self, rule, depth, max_depth):
-        return [self.gen_key(token, depth, max_depth) for token in rule]
-
-    def fuzz(self, key='<start>', max_depth=10):
-        return self.tree_to_str(self.iter_gen_key(key=key, max_depth=max_depth))
-
 
 # ## Compiled Fuzzer
+# 
 # This grammar fuzzer is described in the paper
 # [*Building Fast Fuzzers*](https://rahul.gopinath.org/publications/2019/11/18/arxiv-building/).
 # The idea is to compile a grammar definition to the corresponding source code.
 # Each nonterminal symbol becomes a procedure. First we define a few helpers.
 
-class F1Fuzzer(LimitFuzzer):
+class F1Fuzzer(fuzzer.LimitFuzzer):
     def add_indent(self, string, indent):
         return '\n'.join([indent + i for i in string.split('\n')])
 
