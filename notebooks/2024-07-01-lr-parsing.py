25972f810af0cc35bdf3ef811fbd4c00e65ad54a
Rahul Gopinath <rahul.gopinath@sydney.edu.au>
2024-10-04 23:18:43 +1000

    .

--- notebooks/2024-07-01-lr-parsing.py
+++ notebooks/2024-07-01-lr-parsing.py
@@ -2075,6 +2075,7 @@ if __name__ == '__main__':
         print()
 
 # **Note:** The following resources helped me quite a bit in debugging. [SLR](https://jsmachines.sourceforge.net/machines/slr.html) and [LR](https://jsmachines.sourceforge.net/machines/lr1.html)
+# [checker](http://smlweb.cpsc.ucalgary.ca).
 # 
 # ## References
 # [^grune2008parsing]: Dick Grune and Ceriel J.H. Jacobs "Parsing Techniques A Practical Guide" 2008
